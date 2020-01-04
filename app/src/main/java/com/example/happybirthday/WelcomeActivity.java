0170ecf692687eb6ba836fbb59dd54b25c2964c5
Guy Steinberg <guysteinberg123@gmail.com>
2020-01-04 16:12:55 +0200

    Added Begin Game screen with navigation from main screen

--- app/src/main/java/com/example/happybirthday/WelcomeActivity.java
+++ app/src/main/java/com/example/happybirthday/WelcomeActivity.java
@@ -2,14 +2,24 @@ package com.example.happybirthday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private Button start_game_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_activity);
         getSupportActionBar().hide();
+        start_game_button = findViewById(R.id.start_game_button);
+    }
+
+    public void goToBeginGameScreen(View view){
+        Intent goToBeginGame = new Intent(this, begin_game.class);
+        startActivity(goToBeginGame);
     }
 }
